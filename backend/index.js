f51b9e4d1d9e4e7fe17fb455ecc1b6951325bd1c
zubair <mohammadzubairkhan007@gmail.com>
2023-07-07 17:12:57 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// app.use(express.json());
 
 
 const authRoute = require("./routes/user");
